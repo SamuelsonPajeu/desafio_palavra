fc8c7208e686df40c7a32b9d2c933935ba6798a5
SamuelsonPajeu <samuelson.pajeu@gmail.com>
2022-09-01 20:29:23 -0300

    Update settings.py

--- desafio_palavras_api/settings.py
+++ desafio_palavras_api/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
