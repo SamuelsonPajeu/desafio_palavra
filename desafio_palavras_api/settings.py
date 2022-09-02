5962403888e4130c606501b9817ee0c4188608f9
SamuelsonPajeu <samuelson.pajeu@gmail.com>
2022-09-01 21:47:27 -0300

    Enable CORS

--- desafio_palavras_api/settings.py
+++ desafio_palavras_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'corsheaders',
     'rest_framework',
     'desafio_palavras_app'
 ]
@@ -56,7 +57,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'desafio_palavras_api.urls'
 
